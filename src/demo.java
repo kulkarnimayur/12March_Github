c01107a94e0a24b2018e34a31a34c3ef5a7a7561
Hp <Hp@DESKTOP-2RVD84L>
2022-07-17 10:23:45 -0700

    [Mayur]:change in code

---
+++ src/demo.java
@@ -0,0 +1,11 @@
+
+public class demo 
+{
+public static void main(String[] args) 
+{
+System.out.println("Hello");
+System.out.println("All");
+System.out.println("Mayur");
+System.out.println("Here");
+}
+}
